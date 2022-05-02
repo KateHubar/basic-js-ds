142e4f69baa5495678b4c063cca8ea0c312f7c79
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-05-03 01:33:11 +0300

    feat: implement remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function removeKFromList(l, k) {
+    let root = l;
+    while (root.value === k) {
+        root = root.next;
+    }
+    let prev = root;
+    let next = root ? root.next : null;
+    while (next) {
+        while (next && next.value === k) {
+            next = next.next;
+            prev.next = next;
+        }
+        prev = next;
+        next = prev ? prev.next : null;
+    }
+    return root;
 }
 
 module.exports = {
-  removeKFromList
+    removeKFromList
 };
